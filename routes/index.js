f3003f26b60f7aed0c63e3cc688aa3a818688d2d
Asthriona <im@asthriona.com>
2019-09-17 06:18:41 +0200

    Setting up new routes

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Asthriona' });
 });
-
+router.get('/keyboard', function(req, res, next) {
+  res.render('kbd/kbdindex', { title: 'Asthriona | Keyboard', layout: 'kbd/kbdlayout' });
+});
+router.get('/keyboard/admin', function(req, res, next) {
+  res.render('kbd/kbdadmin', { title: 'Asthriona | Keyboard', layout: 'kbd/kbdlayout' });
+});
+router.get('/keyboard/client', function(req, res, next) {
+  res.render('kbd/kbdclient', { title: 'Asthriona | Keyboard', layout: 'kbd/kbdlayout' });
+});
+router.get('/keyboard/commands', function(req, res, next) {
+  res.render('kbd/kbdcommand', { title: 'Asthriona | Keyboard', layout: 'kbd/kbdlayout' });
+});
+router.get('/keyboard/faq', function(req, res, next) {
+  res.render('kbd/kbdfaq', { title: 'Asthriona | Keyboard', layout: 'kbd/kbdlayout' });
+});
 module.exports = router;
