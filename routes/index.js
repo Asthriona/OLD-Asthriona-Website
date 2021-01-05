686dd90eefcdafe26b571511f08bb7c13db231bf
Asthriona <im@asthriona.com>
2021-01-05 11:03:23 +0100

    Add random line on home page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+const random = [
+  "Poggu!", "Poggy!", "Pog!", "Kappa",
+  "TriHard", "PogChamp", "4Head", "cmonbruh",
+  "LUL", "OmegaLuL", "hahaa", "/*This line is disabled*/",
+  '<h1 class="headline" style="text-transform: uppercase;">this text is huge<h1>',
+  'console.log("hello world!")', "im a twitch creep :)", "Hello from Speedtest.net!"
+
+]
 router.get('/', function(req, res, next) {
-  res.render('index', { layout: 'layout' });
+  res.render('index', { layout: 'layout', random});
 });
 router.get('/wp-admin', function(req, res, next) {
   res.render('wp-admin', { title: ' | You Got Jebaited!', layout: 'none' });
@@ -16,5 +24,12 @@ router.get('/me', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+
+
+
+
+
 
 
